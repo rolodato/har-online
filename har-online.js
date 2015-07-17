134eef165881c5734aacdabe27283743b8514b8d
Rodrigo López Dato <rodrigo@auth0.com>
2015-07-17 11:52:12 -0300

    Use rawgit instead of GitHub directly, fixes #1

--- har-online.js
+++ har-online.js
@@ -1,9 +1,9 @@
 var GitHubApi = require("github");
-
 var github = new GitHubApi({
     version: "3.0.0",
     protocol: "https"
 });
+var url = require("url");
 
 function harViewerJsonp(harObject) {
     return "onInputData(" + JSON.stringify(harObject) + ");";
@@ -28,7 +28,12 @@ function harUrl(harObject, cb) {
             return cb(err);
         } else {
             var gist = res.files[filename].raw_url;
-            return cb(null, viewerUrl(gist));
+            // Use rawgit instead of direct GitHub link
+            // see https://stackoverflow.com/questions/17341122/link-and-execute-external-javascript-file-hosted-on-github
+            var rawgitUrl = url.parse(gist);
+            rawgitUrl.host = "cdn.rawgit.com";
+            var result = url.format(rawgitUrl);
+            return cb(null, viewerUrl(result));
         }
     });
 }
